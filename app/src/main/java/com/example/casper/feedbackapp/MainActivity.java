be933b91241c7655e3970fa7fbf71c395ccfabad
Casper <casperdtuolsen@gmail.com>
2018-11-23 12:26:32 +0100

    Fjernet hardcoding og tilføjet strings Fixed et issue med pop op keyobard som ødelægger layout.

--- app/src/main/java/com/example/casper/feedbackapp/MainActivity.java
+++ app/src/main/java/com/example/casper/feedbackapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.casper.feedbackapp;
 
+import android.support.design.widget.TabItem;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 
@@ -17,6 +18,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -37,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
        // setSupportActionBar(toolbar);
@@ -113,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
             return rootView;
         }
     }
+
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
