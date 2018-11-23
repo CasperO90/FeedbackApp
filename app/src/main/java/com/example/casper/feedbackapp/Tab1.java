2e0af5192cf67c833f73435cb4479eb501d1ddce
Casper <casperdtuolsen@gmail.com>
2018-11-23 13:32:18 +0100

    Tilføjet linje i manifest, der manglede fra forrige commit.

--- app/src/main/java/com/example/casper/feedbackapp/Tab1.java
+++ app/src/main/java/com/example/casper/feedbackapp/Tab1.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
