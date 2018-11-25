e97dcbb24d6a77f00284c9f5b3d91a3d3d918eaf
s175130 <32563621+s175130@users.noreply.github.com>
2018-11-25 13:26:46 +0100

    Fikset fejl

--- app/src/main/java/com/example/casper/feedbackapp/deltagerDagsordenActivity.java
+++ app/src/main/java/com/example/casper/feedbackapp/deltagerDagsordenActivity.java
@@ -13,45 +13,73 @@ public class deltagerDagsordenActivity extends AppCompatActivity implements OnCl
 
     private Button feedbackBtn, tilbageLederBtn;
 
-    TextView tv, tv1, tv2, tv3, tv4, tv5, tv6, tv7, tv8, tv9;
+    private TextView tv, tv1, tv2, tv3, tv4, tv5, tv6, tv7, tv8, tv9;
 
     LederOpretMoedeActivity id = new LederOpretMoedeActivity();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deltager_dagsorden);
 
-        //Button
+        //Buttons
         feedbackBtn = findViewById(R.id.feedbackBtn);
         feedbackBtn.setOnClickListener(this);
 
         tilbageLederBtn = findViewById(R.id.tilbageLederBtn);
         tilbageLederBtn.setOnClickListener(this);
 
+        tv9 = findViewById(R.id.textView7);
+        tv8 = findViewById(R.id.textView12);
+        tv7 = findViewById(R.id.textView13);
+        tv6 = findViewById(R.id.textView14);
+        tv5 = findViewById(R.id.textView15);
+        tv4 = findViewById(R.id.textView16);
+        tv3 = findViewById(R.id.textView17);
+        tv2 = findViewById(R.id.textView18);
+        tv1 = findViewById(R.id.textView19);
+        tv = findViewById(R.id.textView20);
 
-        tv9 = (TextView) findViewById(R.id.textView7);
-        tv8 = (TextView) findViewById(R.id.textView12);
-        tv7 = (TextView) findViewById(R.id.textView13);
-        tv6 = (TextView) findViewById(R.id.textView14);
-        tv5 = (TextView) findViewById(R.id.textView15);
-        tv4 = (TextView) findViewById(R.id.textView16);
-        tv3 = (TextView) findViewById(R.id.textView17);
-        tv2 = (TextView) findViewById(R.id.textView18);
-        tv1 = (TextView) findViewById(R.id.textView19);
-        tv = (TextView) findViewById(R.id.textView20);
+        checkTextTOM();
+    }
 
+    @Override
+    public void onClick(View view) {
 
-        // textviews der står tekst i hvis brugeren har oprettet dem
+        //videre knap
+        if (view == feedbackBtn) {
+            startFeedback();
+        }
+
+        //tilbage knap
+        if (view == tilbageLederBtn) {
+            tilbage();
+            finish();
+        }
+    }
+
+    //intents
+    public void startFeedback() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void tilbage() {
+        Intent intent = new Intent(this, MoedeDeltagActivity.class);
+        startActivity(intent);
+    }
+
+    // checker om textviews er tomme.
+    public void checkTextTOM() {
+
         if (id.text == null) {
-            Log.d("test", "IF");
             tv.setText("");
         } else if (id.text.equals("")) {
-            Log.d("test", "ELSE IF");
             tv.setText("");
-        } else {
-            Log.d("test", "ELSE ");
+        }
+
+
+        else {
             tv.setText("Punkt 1: " + id.text);
         }
 
@@ -63,89 +91,103 @@ public class deltagerDagsordenActivity extends AppCompatActivity implements OnCl
             tv1.setText("Punkt 2: " + id.text1);
         }
 
-
         if (id.text2 == null) {
             tv2.setText("");
-        } else if (id.text2.equals("")) {
+        }
+
+        else if (id.text2.equals("")) {
             tv2.setText("");
-        } else {
+        }
+
+        else {
             tv2.setText("Punkt 3: " + id.text2);
         }
 
         if (id.text3 == null) {
             tv3.setText("");
+        }
 
-        } else if (id.text3.equals("")) {
+        else if (id.text3.equals("")) {
             tv3.setText("");
-        } else {
+        }
+
+        else {
             tv3.setText("Punkt 4: " + id.text3);
         }
 
-
         if (id.text4 == null) {
             tv4.setText("");
-
-        } else if (id.text4.equals("")) {
-            tv4.setText("");
-        } else {
-            tv4.setText("Punkt 5: " + id.text4);
         }
 
+        else if (id.text4.equals("")) {
+            tv4.setText("");
+        }
+
+        else {
+            tv4.setText("Punkt 5: " + id.text4);
+        }
 
         if (id.text5 == null) {
             tv5.setText("");
 
-        } else if (id.text5.equals("")) {
-            tv5.setText("");
-        } else {
-            tv5.setText("Punkt 6: " + id.text5);
         }
 
+        else if (id.text5.equals("")) {
+            tv5.setText("");
+        }
+
+        else {
+            tv5.setText("Punkt 6: " + id.text5);
+        }
 
         if (id.text6 == null) {
             tv6.setText("");
 
-        } else if (id.text6.equals("")) {
-            tv6.setText("");
-        } else {
-            tv6.setText("Punkt 7: " + id.text6);
         }
 
+        else if (id.text6.equals("")) {
+            tv6.setText("");
+        }
+
+        else {
+            tv6.setText("Punkt 7: " + id.text6);
+        }
 
         if (id.text7 == null) {
             tv7.setText("");
 
-        } else if (id.text7.equals("")) {
+        }
+
+        else if (id.text7.equals("")) {
             tv7.setText("");
-        } else {
+        }
+
+        else {
             tv7.setText("Punkt 8: " + id.text7);
         }
 
-
         if (id.text8 == null) {
             tv8.setText("");
+        }
 
-        } else if (id.text8.equals("")) {
+        else if (id.text8.equals("")) {
             tv8.setText("");
-        } else {
+        }
+
+        else {
             tv8.setText("Punkt 9: " + id.text8);
         }
 
-
         if (id.text9 == null) {
             tv9.setText("");
-
-        } else if (id.text9.equals("")) {
-            tv9.setText("");
-        } else {
-            tv9.setText("Punkt 10: " + id.text9);
         }
 
+        else if (id.text9.equals("")) {
+            tv9.setText("");
+        }
 
-    }
-
-    @Override
-    public void onClick(View view) {
-
+        else {
+            tv9.setText("Punkt 10: " + id.text9);
+        }
     }
 }
