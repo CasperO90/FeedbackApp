2d7dd3c29c42c5fda9b269ef12f53910d54dd82e
s175130 <32563621+s175130@users.noreply.github.com>
2018-11-25 13:15:42 +0100

    Ryddet lidt op og prøvet forskellige farver til tema

--- app/src/main/java/com/example/casper/feedbackapp/deltagerDagsordenActivity.java
+++ app/src/main/java/com/example/casper/feedbackapp/deltagerDagsordenActivity.java
@@ -13,10 +13,9 @@ public class deltagerDagsordenActivity extends AppCompatActivity implements OnCl
 
     private Button feedbackBtn, tilbageLederBtn;
 
-    TextView tv,tv1,tv2,tv3,tv4,tv5,tv6,tv7,tv8,tv9;
+    TextView tv, tv1, tv2, tv3, tv4, tv5, tv6, tv7, tv8, tv9;
 
     LederOpretMoedeActivity id = new LederOpretMoedeActivity();
-
 
 
     @Override
@@ -32,208 +31,121 @@ public class deltagerDagsordenActivity extends AppCompatActivity implements OnCl
         tilbageLederBtn.setOnClickListener(this);
 
 
-
-        tv9 = (TextView)findViewById(R.id.textView7);
-        tv8 = (TextView)findViewById(R.id.textView12);
-        tv7 = (TextView)findViewById(R.id.textView13);
-        tv6 = (TextView)findViewById(R.id.textView14);
-        tv5 = (TextView)findViewById(R.id.textView15);
-        tv4 = (TextView)findViewById(R.id.textView16);
-        tv3 = (TextView)findViewById(R.id.textView17);
-        tv2 = (TextView)findViewById(R.id.textView18);
-        tv1 = (TextView)findViewById(R.id.textView19);
-        tv = (TextView)findViewById(R.id.textView20);
-
+        tv9 = (TextView) findViewById(R.id.textView7);
+        tv8 = (TextView) findViewById(R.id.textView12);
+        tv7 = (TextView) findViewById(R.id.textView13);
+        tv6 = (TextView) findViewById(R.id.textView14);
+        tv5 = (TextView) findViewById(R.id.textView15);
+        tv4 = (TextView) findViewById(R.id.textView16);
+        tv3 = (TextView) findViewById(R.id.textView17);
+        tv2 = (TextView) findViewById(R.id.textView18);
+        tv1 = (TextView) findViewById(R.id.textView19);
+        tv = (TextView) findViewById(R.id.textView20);
 
 
-            // textviews der står tekst i hvis brugeren har oprettet dem
-        if(id.text == null){
-            Log.d("test","IF");
+        // textviews der står tekst i hvis brugeren har oprettet dem
+        if (id.text == null) {
+            Log.d("test", "IF");
             tv.setText("");
-        }
-        else if(id.text.equals("")){
-            Log.d("test","ELSE IF");
-                tv.setText("");
-            }
-        else {
-            Log.d("test","ELSE ");
-            tv.setText("Punkt 1: " +id.text);
+        } else if (id.text.equals("")) {
+            Log.d("test", "ELSE IF");
+            tv.setText("");
+        } else {
+            Log.d("test", "ELSE ");
+            tv.setText("Punkt 1: " + id.text);
         }
 
-        if(id.text1 == null){
+        if (id.text1 == null) {
             tv1.setText("");
-        }
-
-        else if(id.text1.equals("")) {
+        } else if (id.text1.equals("")) {
             tv1.setText("");
+        } else {
+            tv1.setText("Punkt 2: " + id.text1);
         }
 
-        else {
-            tv1.setText("Punkt 2: " +id.text1);
-        }
 
-
-        if(id.text2 == null){
+        if (id.text2 == null) {
             tv2.setText("");
-            }
-
-        else if(id.text2.equals("")) {
+        } else if (id.text2.equals("")) {
             tv2.setText("");
+        } else {
+            tv2.setText("Punkt 3: " + id.text2);
         }
 
-
-        else {
-            tv2.setText("Punkt 3: " +id.text2);
-        }
-
-        if(id.text3 == null){
+        if (id.text3 == null) {
             tv3.setText("");
 
-        }
-
-        else if(id.text3.equals("")) {
+        } else if (id.text3.equals("")) {
             tv3.setText("");
+        } else {
+            tv3.setText("Punkt 4: " + id.text3);
         }
 
-        else {
-            tv3.setText("Punkt 4: " +id.text3);
-        }
 
-
-
-        if(id.text4 == null){
+        if (id.text4 == null) {
             tv4.setText("");
 
-        }
-
-        else if(id.text4.equals("")) {
+        } else if (id.text4.equals("")) {
             tv4.setText("");
+        } else {
+            tv4.setText("Punkt 5: " + id.text4);
         }
 
 
-        else {
-            tv4.setText("Punkt 5: " +id.text4);
-        }
-
-
-
-        if(id.text5 == null){
+        if (id.text5 == null) {
             tv5.setText("");
 
-        }
-
-        else if(id.text5.equals("")) {
+        } else if (id.text5.equals("")) {
             tv5.setText("");
+        } else {
+            tv5.setText("Punkt 6: " + id.text5);
         }
 
 
-        else {
-            tv5.setText("Punkt 6: " +id.text5);
-        }
-
-
-        if(id.text6 == null){
+        if (id.text6 == null) {
             tv6.setText("");
 
-        }
-
-        else if(id.text6.equals("")) {
+        } else if (id.text6.equals("")) {
             tv6.setText("");
+        } else {
+            tv6.setText("Punkt 7: " + id.text6);
         }
 
-        else {
-            tv6.setText("Punkt 7: " +id.text6);
-        }
 
-
-        if(id.text7 == null){
+        if (id.text7 == null) {
             tv7.setText("");
 
-        }
-
-        else if(id.text7.equals("")) {
+        } else if (id.text7.equals("")) {
             tv7.setText("");
+        } else {
+            tv7.setText("Punkt 8: " + id.text7);
         }
 
-        else {
-            tv7.setText("Punkt 8: " +id.text7);
-        }
 
-
-
-        if(id.text8 == null){
+        if (id.text8 == null) {
             tv8.setText("");
 
-        }
-
-        else if(id.text8.equals("")) {
+        } else if (id.text8.equals("")) {
             tv8.setText("");
+        } else {
+            tv8.setText("Punkt 9: " + id.text8);
         }
 
-        else {
-            tv8.setText("Punkt 9: " +id.text8);
-        }
 
-
-        if(id.text9 == null){
+        if (id.text9 == null) {
             tv9.setText("");
 
-        }
-
-        else if(id.text9.equals("")) {
+        } else if (id.text9.equals("")) {
             tv9.setText("");
+        } else {
+            tv9.setText("Punkt 10: " + id.text9);
         }
-
-        else {
-            tv9.setText("Punkt 10: " +id.text9);
-        }
-
-
-
-
-
-
-
-
-
-
 
 
     }
-
-
 
     @Override
     public void onClick(View view) {
-        if(view == feedbackBtn){
-                startFeedback();
-            }
 
-        if(view == tilbageLederBtn){
-            tilbage();
-        }
-
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-    public void startFeedback() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    public void tilbage() {
-        Intent intent = new Intent(this, MoedeDeltagActivity.class);
-        startActivity(intent);
     }
 }
